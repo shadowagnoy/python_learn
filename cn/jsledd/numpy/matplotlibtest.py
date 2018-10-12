2fd8aa61ed424f718d4c8447a63926e011de37a8
shadowagnoy <315134590@qq.com>
2018-10-12 20:22:32 +0800

    【新增】python numpy matplotlib

---
+++ cn/jsledd/numpy/matplotlibtest.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(1, 11)
+y = 2 * x + 5
+plt.title("Matplotlib demo")
+plt.xlabel("x axis caption")
+plt.ylabel("y axis caption")
+plt.plot(x, y)
+plt.show()
+# 计算正弦和余弦曲线上的点的 x 和 y 坐标
+x = np.arange(0, 3 * np.pi, 0.1)
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+# 建立 subplot 网格，高为 2，宽为 1
+# 激活第一个 subplot
+plt.subplot(2, 1, 1)
+# 绘制第一个图像
+plt.plot(x, y_sin)
+plt.title('Sine')
+# 将第二个 subplot 激活，并绘制第二个图像
+plt.subplot(2, 1, 2)
+plt.plot(x, y_cos)
+plt.title('Cosine')
+# 展示图像
+plt.show()
