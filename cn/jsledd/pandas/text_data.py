8d053898e884c9ed741ca2fe819c49abf26dda75
shadowagnoy <315134590@qq.com>
2018-10-15 18:04:57 +0800

    【新增】python pands 字符串操作

---
+++ cn/jsledd/pandas/text_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+s = pd.Series(['Tom', 'William Rick', 'John', 'Alber@t', np.nan, '1234', 'SteveMinsu'])
+print(s.str.lower())
+print(s.str.upper())
+print(s.str.len())
+print(s.str.strip())
+print(s.str.split(' '))
+print(s.str.cat(sep=' <=> '))
+print(s.str.get_dummies())
+print(s.str.contains(' '))
+print(s.str.replace('@', '$'))
+print(s.str.repeat(2))
+print(s.str.count('m'))
+print(s.str.startswith('T'))
+print(s.str.endswith('t'))
+print(s.str.find('e'))
+print(s.str.findall('e'))
+print(s.str.swapcase())
+print(s.str.islower())
+print(s.str.isupper())
+print(s.str.isnumeric())
